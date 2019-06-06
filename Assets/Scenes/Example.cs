649e1a8ceb2ae5583a1d89896f091c04d6f4b0f6
chai <huynh.tkim@gmail.com>
2019-06-05 23:56:33 -0600

    Initial push

---
+++ Assets/Scenes/Example.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    public float degrees = 90;
+    public Vector3 rotatePoint;
+    void Update()
+    {
+        // Spin the object around the world origin at 20 degrees/second.
+        transform.RotateAround(rotatePoint, Vector3.up, degrees * Time.deltaTime);
+    }
+}
